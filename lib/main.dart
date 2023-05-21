99b1373a9ae7dedca8533dbb632f48cdbbb1282a
Gagandeep <gagandeepkaur1523@gmail.com>
2023-05-21 18:11:59 +0530

    Added abi file

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.deepPurple,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+          ),
+        ),
+        appBarTheme: const AppBarTheme(elevation: 0),
+      ),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+String infura_url =
+    "https://goerli.infura.io/v3/fffafe76dd334ee2817737b3b3efa2d8";
+
+String owner_private_key =
+    "c12c58827d6c94336bd64cc7480f23cf0808c5c4a46573254e19e5ddb2b02c64";
+
+String voter_private_key =
+    "2f279f40201bcf77d55930005624263fa48769f5d24a66c780e4ba14cb98e778";
